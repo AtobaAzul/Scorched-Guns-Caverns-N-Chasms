1fa142e38a7b287384e688bc99b73910030e5070
Fernando Cercal <atoba.azul.radical@gmail.com>
2025-10-17 10:22:31 -0300

    yay
    Fixed the hexrounds  render crash and gave it trails -Alba

---
+++ src/main/java/net/atobaazul/scguns_cnc/events/client/CnCGunEntityRenderers.java
@@ -0,0 +1,23 @@
+package net.atobaazul.scguns_cnc.events.client;
+
+import net.atobaazul.scguns_cnc.registries.Entities;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import top.ribs.scguns.client.render.entity.ProjectileRenderer;
+
+import static net.atobaazul.scguns_cnc.SCGunsCnC.MOD_ID;
+
+
+@Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class CnCGunEntityRenderers {
+
+    @SubscribeEvent
+    public static void registerEntityRenders(EntityRenderersEvent.RegisterRenderers event)
+    {
+
+        event.registerEntityRenderer(Entities.HEX_ROUND_PROJECTILE.get(), ProjectileRenderer::new);
+
+    }
+}
