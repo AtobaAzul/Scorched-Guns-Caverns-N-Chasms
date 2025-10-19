45f6f5eb06bf14edade565b219e92164fbf66ee5
AtobaAzul <atoba.azul.radical@gmail.com>
2025-10-19 20:55:17 -0300

    What the fuck.

---
+++ src/main/java/net/atobaazul/scguns_cnc/client/render/gun/model/AnathemaModel.java
@@ -0,0 +1,30 @@
+package net.atobaazul.scguns_cnc.client.render.gun.model;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.atobaazul.scguns_cnc.events.client.CCSpecialModels;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemDisplayContext;
+import net.minecraft.world.item.ItemStack;
+import top.ribs.scguns.client.render.gun.IOverrideModel;
+import top.ribs.scguns.client.util.RenderUtil;
+
+
+public class AnathemaModel implements IOverrideModel {
+
+    @SuppressWarnings("resource")
+    @Override
+    public void render(float partialTicks, ItemDisplayContext transformType, ItemStack stack, ItemStack parent, LivingEntity entity, PoseStack matrixStack, MultiBufferSource buffer, int light, int overlay) {
+        RenderUtil.renderModel(CCSpecialModels.ANATHEMA_MAIN.getModel(), stack, matrixStack, buffer, light, overlay);
+
+        matrixStack.pushPose();
+        matrixStack.translate(0, -5.8 * 0.0625, 0);
+        matrixStack.popPose();
+
+    }
+
+    private double ease(double x) {
+        return 1 - Math.pow(1 - (2 * x), 4);
+    }
+
+}
---
+++ src/main/java/net/atobaazul/scguns_cnc/events/client/SpecialModels.java
@@ -0,0 +1,81 @@
+package net.atobaazul.scguns_cnc.events.client;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.model.BakedModel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ModelEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+import static net.atobaazul.scguns_cnc.SCGunsCnC.MOD_ID;
+
+@Mod.EventBusSubscriber(modid = MOD_ID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
+public enum CCSpecialModels {
+    ANATHEMA_MAIN("anathema/main"),
+    HANGMAN_CARBINE_MAIN("hangman_carbine/main"),
+    KETERIYA_MAIN("keteriya/main");
+
+
+
+
+    /**
+     * The location of an item model in the [MOD_ID]/models/special/[NAME] folder
+     */
+    private final ResourceLocation modelLocation;
+
+    /**
+     * Cached model
+     */
+    private BakedModel cachedModel;
+
+    /**
+     * Sets the model's location
+     *
+     * @param modelName name of the model file
+     */
+    SpecialModels(String modelName)
+    {
+        this.modelLocation = new ResourceLocation(MOD_ID, "special/" + modelName);
+    }
+
+    /**
+     * Gets the model
+     *
+     * @return isolated model
+     */
+    public BakedModel getModel()
+    {
+        if(this.cachedModel == null)
+        {
+            this.cachedModel = Minecraft.getInstance().getModelManager().getModel(this.modelLocation);
+        }
+        return this.cachedModel;
+    }
+
+    /**
+     * Registers the special models into the Forge Model Bakery. This is only called once on the
+     * load of the game.
+     */
+    @SubscribeEvent
+    public static void registerAdditional(ModelEvent.RegisterAdditional event)
+    {
+        for(SpecialModels model : values())
+        {
+            event.register(model.modelLocation);
+        }
+    }
+
+    /**
+     * Clears the cached BakedModel since it's been rebuilt. This is needed since the models may
+     * have changed when a resource pack was applied, or if resources are reloaded.
+     */
+    @SubscribeEvent
+    public static void onBake(ModelEvent.BakingCompleted event)
+    {
+        for(SpecialModels model : values())
+        {
+            model.cachedModel = null;
+        }
+    }
+}
