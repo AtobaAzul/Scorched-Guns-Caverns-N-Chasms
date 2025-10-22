54798643c50e0cbd9682ec3c4062d5080a2bcc5a
AtobaAzul <atoba.azul.radical@gmail.com>
2025-10-22 07:47:46 -0300

    correctly tag ammo items for ammo boxes

--- src/main/java/net/atobaazul/scguns_cnc/datagen/providers/ModItemTagsProvider.java
+++ src/main/java/net/atobaazul/scguns_cnc/datagen/providers/ModItemTagsProvider.java
@@ -4,6 +4,10 @@ package net.atobaazul.scguns_cnc.datagen.providers;
 import net.atobaazul.scguns_cnc.registries.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.NotNull;
@@ -19,10 +23,24 @@ public class ModItemTagsProvider extends net.minecraft.data.tags.ItemTagsProvide
         super(output, lookupProvider, blockTags, MOD_ID, existingFileHelper);
     }
 
+    //damnit ribs
+    private static final TagKey<Item>  PISTOL_AMMO = ItemTags.create(new ResourceLocation("scguns", "pistol_ammo"));
+    private static final TagKey<Item>  RIFLE_AMMO = ItemTags.create(new ResourceLocation("scguns", "rifle_ammo"));
+    private static final TagKey<Item>  SHOTGUN_AMMO = ItemTags.create(new ResourceLocation("scguns", "shotgun_ammo"));
 
     @Override
     protected void addTags(HolderLookup.Provider provider) {
         tag(ModTags.Items.ONE_HANDED_CARBINE)
                 .add(ModItems.REHEARSE.get());
+
+        tag(PISTOL_AMMO)
+                .add(ModItems.COMPACT_HEX_ROUND.get());
+
+        tag(RIFLE_AMMO)
+                .add(ModItems.HEX_ROUND.get());
+
+        tag(SHOTGUN_AMMO)
+                .add(ModItems.BLUNTSHOT.get());
+
     }
 }
