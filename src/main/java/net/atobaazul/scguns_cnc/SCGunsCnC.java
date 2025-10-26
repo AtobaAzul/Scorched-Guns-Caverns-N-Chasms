5320750ca6fc55c2b62cfe80f30a43b703a74894
AtobaAzul <atoba.azul.radical@gmail.com>
2025-10-26 18:02:17 -0300

    fix crash on dedicated servers

--- src/main/java/net/atobaazul/scguns_cnc/SCGunsCnC.java
+++ src/main/java/net/atobaazul/scguns_cnc/SCGunsCnC.java
@@ -55,11 +55,14 @@ public class SCGunsCnC
 
         DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> {
             CCClientHandler.registerClientHandlers(modEventBus);
+            BlueprintScreen.registerLoreOnlyItem(new ResourceLocation(MOD_ID, "gravekeeper_blueprint"), "anathema");
         });
     }
 
     private void commonSetup(final FMLCommonSetupEvent event)
     {
+        GunTierRegistry.register("gravekeeper", 6, "gravekeeper_gun_tier", 4);
+
         ProjectileManager.getInstance().registerFactory(ModItems.COMPACT_HEX_ROUND.get(), (worldIn, entity, weapon, item, modifiedGun) -> new HexRoundProjectileEntity(ModEntities.HEX_ROUND_PROJECTILE.get(), worldIn, entity, weapon, item, modifiedGun));
         ProjectileManager.getInstance().registerFactory(ModItems.HEX_ROUND.get(), (worldIn, entity, weapon, item, modifiedGun) -> new HexRoundProjectileEntity(ModEntities.HEX_ROUND_PROJECTILE.get(), worldIn, entity, weapon, item, modifiedGun));
         ProjectileManager.getInstance().registerFactory(ModItems.BLUNTSHOT.get(), (worldIn, entity, weapon, item, modifiedGun) -> new BluntshotProjectileEntity(ModEntities.BLUNTSHOT.get(), worldIn, entity, weapon, item, modifiedGun));
