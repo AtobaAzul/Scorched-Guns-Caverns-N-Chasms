fadd891453bf220b5fc2f3e38940bb5c5631f28d
Fernando Cercal <atoba.azul.radical@gmail.com>
2026-03-20 12:40:09 -0300

    Recur implementation

--- src/main/java/net/atobaazul/scguns_cnc/registries/ModItems.java
+++ src/main/java/net/atobaazul/scguns_cnc/registries/ModItems.java
@@ -234,6 +234,19 @@ public class ModItems {
             )
     );
 
+    //Iron Partisan
+    public static final RegistryObject<AnimatedGunItem> RECUR = REGISTER.register("recur",
+            () -> new AnimatedGunItem(
+                    new Item.Properties().stacksTo(1).durability(800),
+                    "recur", // Model path
+                    ModSounds.MAG_OUT.get(),        // Reload sound mag out
+                    ModSounds.MAG_IN.get(),         // Reload sound mag in
+                    ModSounds.RELOAD_END.get(),           // Reload sound end
+                    ModSounds.COPPER_GUN_JAM.get(),      // Ejector sound pull
+                    ModSounds.COPPER_GUN_JAM.get()    // Ejector sound release
+            )
+    );
+
 
     //blueprint
     public static final RegistryObject<Item> GRAVEKEEPER_BLUEPRINT = REGISTER.register("gravekeeper_blueprint", () -> new BlueprintItem(new Item.Properties().stacksTo(1)));
