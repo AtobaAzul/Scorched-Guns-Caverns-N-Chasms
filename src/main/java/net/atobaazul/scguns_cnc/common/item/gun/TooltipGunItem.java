05203b63ab66c7e4dd9210f89c3d5d968681af4c
AtobaAzul <atoba.azul.radical@gmail.com>
2026-06-27 16:23:16 -0300

    TooltipGunItem

---
+++ src/main/java/net/atobaazul/scguns_cnc/common/item/gun/TooltipGunItem.java
@@ -0,0 +1,35 @@
+package net.atobaazul.scguns_cnc.common.item.gun;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+import top.ribs.scguns.item.animated.AnimatedGunItem;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public class TooltipGunItem extends AnimatedGunItem {
+    private final String tooltipKey;
+    private final String secondaryTooltipKey;
+
+    public TooltipGunItem(Properties properties, String path, SoundEvent reloadSoundMagOut, SoundEvent reloadSoundMagIn, SoundEvent reloadSoundEnd, SoundEvent boltPullSound, SoundEvent boltReleaseSound, String tooltipKey, @Nullable String secondaryTooltipKey) {
+        super(properties, path, reloadSoundMagOut, reloadSoundMagIn, reloadSoundEnd, boltPullSound, boltReleaseSound);
+        this.tooltipKey = tooltipKey;
+        this.secondaryTooltipKey = secondaryTooltipKey;
+    }
+
+    public void appendHoverText(ItemStack stack, @Nullable Level level, List<Component> tooltip, TooltipFlag flag) {
+        if (this.tooltipKey != null && !this.tooltipKey.isEmpty()) {
+            tooltip.add(Component.translatable(this.tooltipKey).withStyle(ChatFormatting.GRAY).withStyle(ChatFormatting.ITALIC));
+        }
+
+        if (this.secondaryTooltipKey != null && !this.secondaryTooltipKey.isEmpty()) {
+            tooltip.add(Component.translatable(this.secondaryTooltipKey).withStyle(ChatFormatting.DARK_GRAY).withStyle(ChatFormatting.ITALIC));
+        }
+
+        super.appendHoverText(stack, level, tooltip, flag);
+    }
+}
