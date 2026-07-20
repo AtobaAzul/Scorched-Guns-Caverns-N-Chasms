3e44397fdeb35d09059e7cf0e2622ba6a003dc2e
AtobaAzul <atoba.azul.radical@gmail.com>
2026-07-20 01:24:14 -0300

    nothing works and I'm gonna kms

--- src/main/java/net/atobaazul/scguns_cnc/mixin/GunnerMobSpawnerMixin.java
+++ src/main/java/net/atobaazul/scguns_cnc/mixin/GunnerMobSpawnerMixin.java
@@ -2,6 +2,7 @@ package net.atobaazul.scguns_cnc.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.atobaazul.scguns_cnc.common.entity.AbstractGravekeeperGunnerEntity;
 import net.atobaazul.scguns_cnc.common.entity.ai.GhoulGunAttackGoal;
 import net.minecraft.world.entity.PathfinderMob;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,12 +12,21 @@ import top.ribs.scguns.config.GunnerMobSpawner;
 public class GunnerMobSpawnerMixin {
     @WrapMethod(method = "reassessWeaponGoal", remap = false)
     private static void scguns_cnc$reassessWeaponGoal(PathfinderMob mob, Operation<Boolean> original) {
-        boolean hasCustomGunAttackGoal = mob.goalSelector.getAvailableGoals().stream()
-                .anyMatch(goal ->goal.getGoal() instanceof GhoulGunAttackGoal<?>);
-
+        boolean hasCustomGunAttackGoal = mob instanceof AbstractGravekeeperGunnerEntity;
 
         if (!hasCustomGunAttackGoal) {
             original.call(mob);
         }
+    }
+
+    @WrapMethod(method = "hasGunAttackGoal", remap = false)
+    private static boolean scguns_cnc$hasGunAttackGoal(PathfinderMob mob, Operation<Boolean> original) {
+        boolean hasCustomGunAttackGoal = mob instanceof AbstractGravekeeperGunnerEntity;
+
+        if (hasCustomGunAttackGoal) {
+            return true;
+        }
+
+        return original.call(mob);
     }
 }
