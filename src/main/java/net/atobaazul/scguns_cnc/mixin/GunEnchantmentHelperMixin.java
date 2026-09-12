06486fbc1e7a3343e075d9f8957bf7ac36a574ff
AtobaAzul <atoba.azul.radical@gmail.com>
2026-09-11 22:12:45 -0300

    scatterer increased recoil from charge

--- src/main/java/net/atobaazul/scguns_cnc/mixin/GunEnchantmentHelperMixin.java
+++ src/main/java/net/atobaazul/scguns_cnc/mixin/GunEnchantmentHelperMixin.java
@@ -2,14 +2,17 @@ package net.atobaazul.scguns_cnc.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import net.atobaazul.scguns_cnc.SCGunsCnC;
 import net.atobaazul.scguns_cnc.registries.ModItems;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.Mth;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
+import top.ribs.scguns.common.ChargeHandler;
 import top.ribs.scguns.common.Gun;
 import top.ribs.scguns.common.item.gun.RechargeableEnergyGunItem;
-import top.ribs.scguns.item.GunItem;
 import top.ribs.scguns.util.GunEnchantmentHelper;
 
 @Mixin(GunEnchantmentHelper.class)
@@ -22,7 +25,7 @@ public class GunEnchantmentHelperMixin {
             CompoundTag tag = weapon.getOrCreateTag();
             float heatLevel = tag.getFloat("HeatLevel");
 
-            float extra_rate = 1 + ((float) heatLevel / 15);
+            float extra_rate = 1 + (heatLevel / 15);
 
             return (int) Math.floor(rate * extra_rate);
         }
@@ -36,8 +39,21 @@ public class GunEnchantmentHelperMixin {
             float maxDamageMult = 1.0f;
             float minDamageMult = 0.8f;
 
-            return Mth.lerp(chargeProgress, damage*maxDamageMult, damage*minDamageMult);
+            return Mth.lerp(chargeProgress, damage * maxDamageMult, damage * minDamageMult);
         }
-        return original.call(weapon,damage,chargeProgress);
+        return original.call(weapon, damage, chargeProgress);
+    }
+
+    @WrapMethod(method = "getRecoilModifier(Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;)F", remap = false)
+    private static float scguns_cnc$getRecoilModifier(Player player, ItemStack weapon, Operation<Float> original) {
+        float recoil = original.call(player, weapon);
+
+        if (weapon.is(ModItems.SCATTERER.get())) {
+            float chargeProgress = ChargeHandler.getChargeProgress(player, weapon);
+            float extra_recoil = Mth.lerp(chargeProgress, 0, 5);
+
+            return recoil + extra_recoil;
+        }
+        return recoil;
     }
 }
